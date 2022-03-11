ac8007931472ab198661bb489519a2c25ab44922
voidalgorthm <3atlas.navis.noos@gmail.com>
2022-03-11 23:32:40 +0800

    feat: add function to ask user for input

--- script.js
+++ script.js
@@ -1,33 +1,46 @@
 let roundMessage;
 
-function computerPlay(){
+function computerPlay() {
     let computerHand;
-    let handGenerator = Math.floor(Math.random()*3) + 1;
+    let handGenerator = Math.floor(Math.random() * 3) + 1;
 
-    if(handGenerator === 1) return computerHand = "Rock";
-    else if(handGenerator === 2) return computerHand = "Paper";
-    else if(handGenerator === 3) return computerHand = "Scissors";
+    if (handGenerator === 1) return computerHand = "Rock";
+    else if (handGenerator === 2) return computerHand = "Paper";
+    else if (handGenerator === 3) return computerHand = "Scissors";
 }
 
-const playerHand = "RoCk";
+function checkCase(hand) { return hand.toLowerCase(); };
 
-function caseInsensitive(hand) { return hand.toLowerCase(); };
+function getInput() {
+    while (true) {
+        const playerHand = prompt("Hand?", "").toLowerCase();
 
-const playerSelection = caseInsensitive(playerHand);
-const computerSelection = caseInsensitive(computerPlay());
+        if (playerHand === 'rock' || playerHand === 'paper' || playerHand === 'scissors') {
+            return playerHand;
+        } else if (playerHand === null || playerHand === NaN)
+            alert("Impossible! Try again!");
+        else {
+            alert("Invalid hand! Try again!");
+        }
+    }
+}
 
 function playRound(playerSelection, computerSelection) {
-    if(playerSelection === computerSelection) {
+    alert(`Player chose ${playerSelection}`);
+    if (playerSelection === computerSelection) {
         return roundMessage = `It's a tie! ${playerSelection} ties with ${computerSelection}`;
-    } else if (( playerSelection=='rock' && computerSelection=='paper') 
-    || ( playerSelection=='paper' && computerSelection=='scissors')
-    || ( playerSelection=='scissors' && computerSelection=='rock')){
-        return roundMessage = `You lose! ${playerSelection} loses to ${computerSelection}`;        
-    }  else {
+    } else if ((playerSelection == 'rock' && computerSelection == 'paper')
+        || (playerSelection == 'paper' && computerSelection == 'scissors')
+        || (playerSelection == 'scissors' && computerSelection == 'rock')) {
+        return roundMessage = `You lose! ${playerSelection} loses to ${computerSelection}`;
+    } else {
         return roundMessage = `You win! ${playerSelection} beats ${computerSelection}`;
     }
 }
 
-alert(`Player chose ${playerSelection}`);
-playRound(playerSelection, computerSelection)
+const playerSelection = getInput();
+const computerSelection = checkCase(computerPlay());
+
+playRound(playerSelection, computerSelection);
 alert(`${roundMessage}`);
+
