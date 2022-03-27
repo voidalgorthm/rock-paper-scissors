f08685ec9ffaf3d503e1e9fa17b0ced9630c94da
voidalgorthm <3atlas.navis.noos@gmail.com>
2022-03-27 19:51:40 +0800

    fix: modify player hand selection and scoring table for rounds

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-const container = document.querySelector('container');
+const card = document.querySelector('#card')
 const play = document.getElementById('play');
 const next = document.getElementById('next');
 const reset = document.getElementById('reset');
-const buttons = document.getElementsByClassName('button hand disabled');
+const hands = card.querySelectorAll('div.hand-btn > button');
+const stats = document.getElementById('status');
+const score = document.getElementById('score');
 const output = document.getElementById('output');
 
-play.addEventListener('click', game);
+stats.textContent = 'Rock Paper Scissors 5 Rounds';
 output.textContent = 'Click Play to start!!!';
-console.log(buttons);
+play.addEventListener('click', gameStart);
+next.addEventListener('click', game);
 
+let handChosen = false;
+hands.forEach(button => {
+    button.addEventListener('click', selectHand);
+});
+
+let playerSelection;
 let roundMessage;
 let playerScore = 0;
 let computerScore = 0;
 
-function gameStart() {
+
+function gameStart(e) {
+    
+    
     play.disabled = true;
     next.disabled = false;
     next.classList.remove('disabled');
     play.classList.add('disabled');
-    [...buttons].forEach((button) => {
+    hands.forEach(button => {
         button.classList.remove('disabled');
     });
+    output.textContent = 'Choose your hand in the above buttons!!!';
 }
 
 function computerPlay() {
@@ -32,17 +45,6 @@ function computerPlay() {
     else if (handGenerator === 3) return computerHand = "Scissors";
 }
 
-function checkCase(hand) { return hand.toLowerCase(); };
-
-function selectHand(e) {
-
-    buttons.forEach((button) => {
-        button.addEventListener('click', () => {
-            return playerHand = button.textContent;
-        });
-    });
-}
-
 function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === computerSelection) {
@@ -51,30 +53,44 @@ function playRound(playerSelection, computerSelection) {
         || (playerSelection == 'paper' && computerSelection == 'scissors')
         || (playerSelection == 'scissors' && computerSelection == 'rock')) {
         computerScore += 1;
+        console.log(computerScore);
         return roundMessage = `You lose! ${playerSelection} loses to ${computerSelection}`;
     } else {
         playerScore += 1;
+        console.log(playerScore);
         return roundMessage = `You win! ${playerSelection} beats ${computerSelection}`;
     }
 }
 
-function game(e) {
-    gameStart();
+function showScore() {
     const scoreboard = `Player: [${playerScore}] - [${computerScore}] :Computer`;
-    // const playerSelection = dashboard.addEventListener('click', selectHand());
-    console.log('heh');
-    // console.log(playerSelection);
-    const computerSelection = checkCase(computerPlay());
-    // console.log(computerSelection);
-    // console.log(`Player chose ${playerSelection}`);
-    playRound('rock', computerSelection);
-    // output.textContent = roundMessage;
+    score.textContent = scoreboard;
+}
 
-    /* if (playerScore > computerScore) {
+function selectHand(e) {
+    handChosen = true;
+    playerSelection = e.target.value;
+}
+
+function game(e) {
+    if (handChosen) {
+        output.textContent = 'Choose your hand in the above buttons!!!';
+
+        const computerSelection = computerPlay().toLowerCase();
+        stats.textContent = `Player: ${playerSelection} vs. Computer: ${computerSelection}`;
+
+        playRound(playerSelection, computerSelection);
+        showScore();
+        output.textContent = roundMessage;
+    }
+}
+
+function determineWinner(e) {
+    if (playerScore > computerScore) {
         console.log(`The player won, The computer lost!\nHumanity triumphs once again!`);
     } else if (playerScore < computerScore) {
         console.log(`The computer won, The player lost!\nThe rise of the machines!`);
     } else {
         console.log(`Nobody won!, a draw for the player and computer!`);
-    } */
+    }
 }
